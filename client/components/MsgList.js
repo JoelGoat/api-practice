3a6349f56c58bc665f70abe88e991bc6d5025e2f
royalbluec <tkyeom96@gmail.com>
2022-09-07 22:42:42 +0900

    Warning: Text content did not match. Server 오류 처리

--- client/components/MsgList.js
+++ client/components/MsgList.js
@@ -1,16 +1,5 @@
+import { useEffect, useState } from 'react'
 import MsgItem from './MsgItem'
-
-const UserIds = ['roy', 'jay']
-const getRandomUserId = () => UserIds[Math.round(Math.random())]
-
-const msgs = Array(50)
-  .fill(0)
-  .map((_, i) => ({
-    id: 50 - i,
-    userId: getRandomUserId(),
-    timestamp: 1234567890123 + (50 - i) * 1000 * 60,
-    text: `${50 - i} mock text`,
-  }))
 
 // [
 //   {
@@ -21,12 +10,33 @@ const msgs = Array(50)
 //   }
 // ]
 
-const MsgList = () => (
-  <ul className="messages">
-    {msgs.map((x) => (
-      <MsgItem key={x.id} {...x} />
-    ))}
-  </ul>
-)
+const MsgList = () => {
+  const [msgs, setMsgs] = useState([])
+
+  const UserIds = ['roy', 'jay']
+  const getRandomUserId = () => UserIds[Math.round(Math.random())]
+
+  useEffect(() => {
+    setMsgs(
+      Array(50)
+        .fill(0)
+        .map((_, i) => ({
+          id: i + 1,
+          userId: getRandomUserId(),
+          timestamp: 1234567890123 + i * 1000 * 60,
+          text: `${i + 1} mock text`,
+        }))
+        .reverse()
+    )
+  }, [])
+
+  return (
+    <ul className="messages">
+      {msgs.map((x) => (
+        <MsgItem key={x.id} {...x} />
+      ))}
+    </ul>
+  )
+}
 
 export default MsgList
